86f6ec6d773ba7bc09a29a9f1b19ddc29f5aa6cf
Aryan Vishwakarma <aryanongithub@gmail.com>
2023-11-22 19:33:19 +0530

    wedding new images added

--- app/wedding/page.js
+++ app/wedding/page.js
@@ -3,10 +3,10 @@ import EventsLayout from 'components/EventsLayout'
 
 const Wedding = () => {
   const images = [
-    '/wedding/4.jpg',
+    '/wedding/1.jpeg',
     '/wedding/2.jpg',
-    '/wedding/6.jpg',
-    '/wedding/1.jpg',
+    '/wedding/3.jpg',
+    '/wedding/4.jpg',
   ]
   return (
     <EventsLayout primaryTitle='Weddings By Arch' heroTitle='Weddings' secondaryTitle='Full Planning & Premier Design' contentHeading='Every. Single. Detail.' contentParagraph='<br/> We know that on your wedding day, nothing is more important than Every. Single. Detail. </br></br> We make it our mission to guide couples through each step of the wedding planning process to ensure everyone – from newly engaged couples to each individual guest – are happy, carefree and in the moment on the big day.' imgSrc={images}
