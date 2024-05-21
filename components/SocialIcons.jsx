2ff38f7ab56a1dd646c1a5c0b1f617f0d66af1cb
Avgeya India <159917433+github-avegya@users.noreply.github.com>
2024-05-21 15:15:41 +0530

    chore: fixed social-icons

--- components/SocialIcons.jsx
+++ components/SocialIcons.jsx
@@ -7,25 +7,13 @@ import Link from 'next/link'
 
 const SocialIcons = ({ margin }) => {
   return (
-    <div classNameName={`flex items-center justify-center ${margin} relative`}>
-      <div className="group">
-        <AiOutlineInstagram size={30} className='mx-1 cursor-pointer text-[#2C94AD] ' />
-        <div className="hidden group-hover:block p-2 bg-white shadow-xl border absolute rounded-lg left-2  space-y-1 text-[#46a1b7]">
-          <Link href="https://www.instagram.com/eventsbyarch/">
-            @eventsbyarch
-          </Link><br />
-          <Link href="https://www.instagram.com/giftsbyarch/">
-            @giftsbyarch
-          </Link><br />
-          <Link href="https://www.instagram.com/balloonsbyarch/">
-            @balloonsbyarch
-          </Link>
-        </div>
-      </div>
-
-      <Link href="https://facebook.com/eventsbyarch"><AiOutlineFacebook size={30} classNameName='mx-1 cursor-pointer text-[#2C94AD]' /></Link>
-      <Link href="https://www.youtube.com/@eventsbyarch"><AiOutlineYoutube size={30} classNameName='mx-1 cursor-pointer text-[#2C94AD]' /></Link>
-      <Link href="#"><FaTiktok size={30} classNameName='mx-1 cursor-pointer text-[#2C94AD]' /></Link>
+    <div className={`flex items-center justify-center ${margin} relative gap-2`}>
+      <Link href={`https://www.instagram.com/eventsbyarch/`} className='cursor-pointer'>
+        <AiOutlineInstagram color='#2C94AD' size={30} className=' cursor-pointer text-[#2C94AD] ' />
+      </Link>
+      <Link href="https://facebook.com/eventsbyarch"><AiOutlineFacebook color='#2C94AD' size={30} classNameName=' cursor-pointer ' /></Link>
+      <Link href="https://www.youtube.com/@eventsbyarch"><AiOutlineYoutube color='#2C94AD' size={30} classNameName=' cursor-pointer ' /></Link>
+      <Link href="#"><FaTiktok color='#2C94AD' size={30} classNameName=' cursor-pointer ' /></Link>
     </div>
   )
 }
